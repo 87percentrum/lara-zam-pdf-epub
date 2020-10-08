7eb7fea49d9b932247db6be9a1f9712e666ba562
Umut Yalcinkaya <umut@yalcinkaya.ninja>
2020-10-08 18:12:06 +1100

    added web route

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('main');
+});
+
+Route::get('/files/{file}', function($file) {
+    $file_path = storage_path('public/'.$file);
+    return response()->file($file_path);
+});
