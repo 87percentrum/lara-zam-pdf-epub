0e0e2cab2c39a5ddf23b877e7554adce7e3efa2e
Umut Yalcinkaya <umut@yalcinkaya.ninja>
2020-10-08 18:23:59 +1100

    removed cdn

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}"/>
-
-    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
-
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     <title>PDF => EPUB</title>
 </head>
